8d017f2ee8aa4d61c31d97a31497c251daf39421
blubrandi <blubrandi0@gmail.com>
2019-01-24 21:30:59 -0700

    Completed MVP.

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -9,37 +9,37 @@ Prototype Refactor
 
 */
 
-function GameObject(attr){
-    this.createdAt = attr.createdAt
-    this.dimensions = attr.dimensions
-  }
+// function GameObject(attr){
+//     this.createdAt = attr.createdAt
+//     this.dimensions = attr.dimensions
+//   }
   
-  GameObject.prototype.destroy = function() {
-    return `${this.name} was removed from the game.`
-  }
+//   GameObject.prototype.destroy = function() {
+//     return `${this.name} was removed from the game.`
+//   }
 
-  function CharacterStats(stats) {
-    GameObject.call(this,stats)
-    this.healthPoints = stats.healthPoints
-    this.name = stats.name
-  }
+//   function CharacterStats(stats) {
+//     GameObject.call(this,stats)
+//     this.healthPoints = stats.healthPoints
+//     this.name = stats.name
+//   }
   
-  CharacterStats.prototype = Object.create(GameObject.prototype)
+//   CharacterStats.prototype = Object.create(GameObject.prototype)
   
-  CharacterStats.prototype.takeDamage = function() {
-    return `${this.name} took damage.`
-  }
+//   CharacterStats.prototype.takeDamage = function() {
+//     return `${this.name} took damage.`
+//   }
 
-  function Humanoid(info) {
-    CharacterStats.call(this,info)
-    this.team = info.team
-    this.weapons = info.weapons
-    this.language = info.language
-  }
+//   function Humanoid(info) {
+//     CharacterStats.call(this,info)
+//     this.team = info.team
+//     this.weapons = info.weapons
+//     this.language = info.language
+//   }
   
-  Humanoid.prototype = Object.create(CharacterStats.prototype)
+//   Humanoid.prototype = Object.create(CharacterStats.prototype)
   
-  Humanoid.prototype.greet = function() {
-    return `${this.name} offers a greeting in ${this.language}`
-  }
+//   Humanoid.prototype.greet = function() {
+//     return `${this.name} offers a greeting in ${this.language}`
+//   }
   
